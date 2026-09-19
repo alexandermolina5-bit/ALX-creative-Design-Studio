ee6fbe2668af9ef8751bb8c5582b60e9080cd89b
alexandermolina5-bit <alexander.molina5@gmail.com>
2026-09-20 01:32:00 +0200

    Fix Java and Kotlin JVM target compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,6 +12,15 @@ android {
         versionName = "5.3-ready"
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
